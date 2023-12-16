3e95800fd03db52b6d5260e5749bfb6fd1e90d04
jodyfleck <jody_fleck@hotmail.com>
2023-12-16 12:08:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
+LETS FREAKING GO BUDDY, LETS FREAKING DO THIS.
+
 Do some cool things.///
 
 In the meantime, below is an example of what you can do with just a few lines of code:
